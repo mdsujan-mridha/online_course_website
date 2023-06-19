4334f33157d481fe4f8271b5c9db79bd703c0d85
mdsujan-mriddha <mdsujan134523@gmail.com>
2023-06-19 12:30:56 +0600

    create register page

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "387b7e84f32b9f87"; }
+/******/ 		__webpack_require__.h = function() { return "3ef3f999a3041006"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/pages/register.js
+++ client/pages/register.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const register = () => {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log({name,email,password});
+    }
     return (
-        <div>
-            <h1 className='jumbotron text-center bg-primary square py-4'> Register </h1>
-        </div>
+        <>
+            <h1 className='jumbotron text-center bg-primary square py-4 text-white'> Register </h1>
+            <div className="container col-md-4 offset-md-4 pb-5">
+                <form onSubmit={handleSubmit}>
+                    <input type="text" className='form-control mb-4 p-4' 
+                    value={name} 
+                    onChange={(e) => setName(e.target.value)} 
+                    placeholder='Enter your name'
+                    required
+                    />
+                    {/* email  */}
+                    <input type="email" className='form-control mb-4 p-4' 
+                    value={email} 
+                    onChange={(e) => setEmail(e.target.value)} 
+                    placeholder='Enter your email'
+                    required
+                    />
+                    {/* password  */}
+                    <input type="password" className='form-control mb-4 p-4' 
+                    value={password} 
+                    onChange={(e) => setPassword(e.target.value)} 
+                    placeholder='Enter your password'
+                    required
+                    />
+                 
+                    <button type= "submit" className='btn btn-block btn-primary'>Submit </button>
+                </form>
+            </div>
+        </>
     );
 };
 
